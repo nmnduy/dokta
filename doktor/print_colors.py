32bcde942692a64ac600670b2967b5d02e64ba11
duy <nmnduy@gmail.com>
2024-02-01 13:58:08 -0600

    flush on print yellow

--- doktor/print_colors.py
+++ doktor/print_colors.py
@@ -2,7 +2,7 @@ def print_green(text, newline=True):
     print(f"\033[32m{text}\033[0m", end="\n" if newline else "")
 
 def print_yellow(text, newline=True):
-    print(f"\033[33m{text}\033[0m", end="\n" if newline else "")
+    print(f"\033[33m{text}\033[0m", end="\n" if newline else "", flush=True)
 
 def print_red(text):
     print(f"\033[31m{text}\033[0m")
