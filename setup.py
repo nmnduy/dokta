408977fda51d461fc7dba675302db3d4db97ecf7
duy <nmnduy@gmail.com>
2024-04-01 19:02:18 +0700

    fix cannot install

--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
 from setuptools import setup, find_packages
-import pip
-
-
-try:
-    # check if wheel is installed   
-    __import__('wheel')
-except ImportError:
-    # if not, install it
-    pip.main(['install', 'wheel'])
-
 setup(
     name="dokta",
     version="0.1.0",
